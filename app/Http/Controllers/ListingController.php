147ca3ae4720a77e1306172ef8e0cf431f521756
Sergey Kulichkin <s.kulichkin@gmail.com>
2023-05-22 21:58:32 +0500

    Update listings

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -52,4 +52,33 @@ class ListingController extends Controller
 
         return redirect('/')->with('message', 'Listing created successfully!');
     }
+
+    // Show Edit Form
+    public function edit(Listing $listing)
+    {
+        return view('listings.edit', ['listing' => $listing]);
+    }
+
+
+    // Update Listing Data
+    public function update(Request $request, Listing $listing)
+    {
+        $formFields = $request->validate([
+            'title' => 'required',
+            'company' => ['required', Rule::unique('listings', 'company')],
+            'location' => 'required',
+            'website' => 'required',
+            'email' => ['required', 'email'],
+            'tags' => 'required',
+            'description' => 'required'
+        ]);
+
+        if ($request->hasFile('logo')) {
+            $formFields['logo'] = $request->file('logo')->store('logos', 'public');
+        }
+
+        $listing->update($formFields);
+
+        return back()->with('message', 'Listing updated successfully!');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -25,5 +25,8 @@ Route::post('/listings', [ListingController::class, 'store']);
 // Show Edit Form
 Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
 
+// Update Listing
+Route::put('/listings/{listing}', [ListingController::class, 'update']);
+
 // Single Listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
